081a7c62e2389d5a57d827c7698e818363b6339d
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-08-18 00:18:08 +0300

    commit 1

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-<<<<<<< HEAD
-import javax.validation.ValidationException;
-import java.util.Objects;
-
-=======
->>>>>>> fa10711 (commit 1)
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
@@ -45,20 +39,11 @@ public class ErrorHandler {
         return new ErrorResponse("Validation error 409", e.getMessage());
     }
 
-<<<<<<< HEAD
-    @ExceptionHandler(Throwable.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException(final Throwable e) {
-        String error = "Ошибка: " + e.getClass() + ", " + e.getMessage() + ", причина: " + Objects.toString(e.getCause().getMessage(), "");
-        log.error(error);
-        return new ErrorResponse(error);
-=======
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidate(final BookingException e) {
         log.error("Validation Exception");
         return new ErrorResponse("Validation error 400", e.getMessage());
->>>>>>> fa10711 (commit 1)
     }
 
     @ExceptionHandler
