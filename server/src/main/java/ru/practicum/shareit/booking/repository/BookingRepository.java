7813b8717b2d990e91b5bd3852c1d82fafdc127b
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-08-29 04:50:35 +0300

    commit 4 - codestyle 2

--- server/src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ server/src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -12,39 +12,26 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    //ALL
     List<Booking> findAllByBookerIdOrderByIdDesc(Long userId, Pageable page);
 
-    //PAST
     List<Booking> findAllByBookerIdAndEndBeforeOrderByIdDesc(Long userId, LocalDateTime now, Pageable page);
 
-    //FUTURE
     List<Booking> findAllByBookerIdAndStartAfterOrderByIdDesc(Long userId, LocalDateTime now, Pageable page);
 
-    //CURRENT
     List<Booking> findAllByBookerIdAndStartBeforeAndEndAfterOrderById(Long userId, LocalDateTime now,
                                                                       LocalDateTime now1, Pageable page);
 
-    //WAITING
-    //REJECTED
     List<Booking> findAllByBookerIdAndStatusOrderById(Long userId, Status status, Pageable page);
 
-    //BY OWNER
-    //All
     List<Booking> findBookingsByItem_Owner_IdOrderByStartDesc(Long bookerId, Pageable page);
 
-    //PAST
     List<Booking> findBookingsByItem_Owner_IdAndEndIsBefore(Long bookerId, LocalDateTime now, Pageable page);
 
-    //FUTURE
     List<Booking> findBookingsByItem_Owner_IdAndStartIsAfter(Long bookerId, LocalDateTime now,Pageable page);
 
-    //CURRENT
     List<Booking> findBookingsByItem_Owner_IdAndStartIsBeforeAndEndIsAfter(Long bookerId, LocalDateTime now,
                                                                            LocalDateTime now1, Pageable page);
 
-    //WAITING
-    //REJECTED
     List<Booking> findBookingsByItem_Owner_IdAndStatusEquals(Long bookerId, Status status, Pageable page);
 
     Optional<Booking> findFirstByItemIdAndStartAfterAndStatusOrderByStartAsc(Long itemId, LocalDateTime now, Status approved);
